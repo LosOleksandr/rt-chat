5ff681f80f86df3d5d4147616278b1b7a59d1075
LosOleksandr <losalexander2003@gmail.com>
2024-01-05 22:39:22 +0200

    fix: remove unused imports

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,7 @@
-import { useSession } from "next-auth/react";
-import Image from "next/image";
 import React from "react";
 
 const Header = () => {
-  const { data: session } = useSession();
-
-  return (
-    <header className="absolute top-0 right-0 p-4">
-      
-    </header>
-  );
+  return <header className="absolute top-0 right-0 p-4"></header>;
 };
 
 export default Header;
